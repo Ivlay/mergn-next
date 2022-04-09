3ec7466ae448e39b82a660e2e3b9bc78adb2a175
vampi <vampir015@bk.ru>
2022-04-09 18:00:48 +0500

    create login page

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -1,12 +1,13 @@
 import { memo } from 'react';
 import styled, { DefaultTheme, StyledComponentProps } from 'styled-components';
+import { forwardRef } from 'react';
 
 const InputStyle = styled.input`
   height: 48px;
   width: 100%;
   border: none;
-  border-bottom: 1px solid ${props => props.theme.color};
-  color: ${props => props.theme.color};
+  border-bottom: 1px solid ${(props) => props.theme.color};
+  color: ${(props) => props.theme.color};
   background-color: transparent;
   padding: 9px 16px;
 
@@ -14,19 +15,21 @@ const InputStyle = styled.input`
   &:-webkit-autofill:hover,
   &:-webkit-autofill:focus,
   &:-webkit-autofill:active {
-      transition: color 9999s ease-out, background-color 9999s ease-out;
-      transition-delay: 9999s;
+    transition: color 9999s ease-out, background-color 9999s ease-out;
+    transition-delay: 9999s;
   }
 
   &:focus {
-      outline: none;
-      border-color: ${props => props.theme.button.gradient[1]};
-      box-shadow: 0 23px 32px -22px ${props => props.theme.button.gradient[1]}
+    outline: none;
+    border-color: ${(props) => props.theme.button.gradient[1]};
+    box-shadow: 0 23px 32px -22px ${(props) => props.theme.button.gradient[1]};
   }
 `;
 
-const Input: React.FC<StyledComponentProps<'input', DefaultTheme, {}, never>> = ({ ...rest }) => {
-  return <InputStyle {...rest} />;
-};
+const Input: React.FC<StyledComponentProps<'input', DefaultTheme, {}, never>> = forwardRef(
+  ({ ...rest }, ref) => {
+    return <InputStyle ref={ref} {...rest} />;
+  },
+);
 
 export default memo(Input);
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,25 +1,61 @@
 import { NextPage } from 'next';
 import { useMutation } from '@apollo/client';
-import { Button } from 'src/components/UI';
+import { Input } from 'src/components/UI';
 import { LOGIN } from 'graphql/Post';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface IFormInput {
+  login: string;
+  password: string;
+}
 
 const Login: NextPage = () => {
+  const INPUTS = [
+    {
+      name: 'username',
+      required: true,
+      type: 'text',
+      id: 1,
+    },
+    {
+      name: 'password',
+      required: true,
+      type: 'password',
+      id: 2,
+    },
+  ];
+
   const [getPosts, { data }] = useMutation(LOGIN);
 
-  console.log('data :>> ', data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
   const loadPost = () => {
-    console.log('qweqweqwe');
-
     getPosts();
   };
 
   return (
     <div>
-      Login page
-      <Button onClick={loadPost}> 
-        Load posts
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {INPUTS.map((itemInput) => {
+          return (
+            <Input
+              placeholder={itemInput.name}
+              key={itemInput.id}
+              name={itemInput.name}
+              type={itemInput.type}
+              {...register(itemInput.name, { required: true })}
+            />
+          );
+        })}
+        {errors.username || errors.password ? <span>This field is required</span> : <></>}
+        <Input type="submit"/>
+      </form>
     </div>
   );
 };
